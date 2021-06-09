c0e3d9edc7ee1d0cbb0d6a3335138ed0b8788a8e
Caroline Oliveira <carolinevsboliveira@gmail.com>
2021-06-08 21:45:36 -0400

    Solução do exercício Me segue no Insta!

---
+++ me_segue_no_insta.js
@@ -0,0 +1,5 @@
+function solucao(largura, altura) {
+  if (largura > altura) console.log("PAISAGEM");
+  if (altura > largura) console.log("RETRATO");
+  if (altura == largura) console.log("QUADRADA");
+}
